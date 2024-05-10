66411ac154783c1dfebe967618bd7f4de9b1aee5
gb10udn <rbbpx0926@gmail.com>
2024-05-10 19:56:18 +0900

    add function to download ffmpeg and show download status

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,56 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use std::path::Path;
+use std::fs;
+use reqwest::blocking::Client;
+use zip::read::ZipArchive;
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+const FFMPEG_PATH: &str = "./misc/ffmpeg.exe";
+const FFMPEG_URL: &str = "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl.zip";
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![download_ffmpeg, check_ffmpeg_downloaded])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+
+#[tauri::command]
+fn download_ffmpeg() -> Result<(), String> {
+    if !Path::new(FFMPEG_PATH).exists() {
+        println!("Start to download ffmpeg ...");
+        let temp_dir = Path::new("./temp_ffmpeg");
+        fs::create_dir_all(temp_dir).expect("Fail to create temp directory ...");
+
+        let client = Client::new();
+        let mut response = client.get(FFMPEG_URL).send().expect(format!("Fail to access -> {} ...", FFMPEG_URL).as_str());
+        if response.status().is_success() {
+            let mut dst = fs::File::create(temp_dir.join("ffmpeg.zip")).expect("Fail ...");
+            response.copy_to(&mut dst).expect("Fail ...");
+
+            let zip_path = temp_dir.join("ffmpeg.zip");
+            let target_dir = temp_dir.join("ffmpeg");
+            let mut archive = ZipArchive::new(fs::File::open(zip_path).expect("Fail ...")).expect("Fail ...");
+            archive.extract(&target_dir).expect("Fail ...");
+
+            let src_path = target_dir.join("ffmpeg-master-latest-win64-gpl/bin/ffmpeg.exe");
+
+            if let Some(dst_dir) = Path::new(FFMPEG_PATH).parent() {
+                fs::create_dir_all(dst_dir).unwrap();
+            }
+
+            fs::copy(src_path, FFMPEG_PATH).expect("Fail to copy ...");
+            fs::remove_dir_all(temp_dir).expect("Fail to remove temp directory ...");
+        } else {
+            println!("Failed to download FFmpeg.");
+        }
+    }
+    Ok(())
+}
+
+
+#[tauri::command]
+fn check_ffmpeg_downloaded() -> bool {
+    Path::new(FFMPEG_PATH).exists()
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,38 @@
 import Uploader from "./components/Uploader.tsx";
 import Canvas from "./components/Canvas.tsx";
-import { useRef } from "react";
+import { useRef, useState, useEffect } from "react";
+import { invoke } from "@tauri-apps/api/tauri";
 
-function App() {
+const App = () => {
   const videoRef = useRef<HTMLVideoElement>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [downloaded, setDownloaded] = useState<boolean>(false);
+
+  useEffect(() => {
+    invoke("check_ffmpeg_downloaded", {}).then((result) => {
+      setDownloaded(result as boolean);
+      if (result === true) {
+        console.log("already downloaded ffmpeg !!!")
+      } else {
+        invoke("download_ffmpeg", {}).then(() => {
+          console.log("Finished downloading ffmpeg !!!");
+          setDownloaded(true);
+        }).catch((err) => {
+          console.log(err);
+          setDownloaded(false);
+        });      
+      }
+    }).catch((err) => {
+      console.log(err);
+      setDownloaded(false);
+    })
+  }, []);
 
   return (
     <div className="mx-3 mt-3">
       <h1 className="text-3xl font-bold">ffmpeg wrapper gui</h1>
+      {/* TODO: 240510 もう少し直感的に理解できそうな表記にする */}
+      <p>ffmpeg downloaded → {String(downloaded)} </p>
       <Uploader videoRef={videoRef}/>
       <div className="mt-5"></div>
       <Canvas canvasRef={canvasRef} videoRef={videoRef} />
