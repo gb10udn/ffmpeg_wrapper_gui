728fc31010175b17c493872e0da367074868f808
gb10udn <rbbpx0926@gmail.com>
2024-05-12 15:10:18 +0900

    minor fix css

--- src/components/Controller.tsx
+++ src/components/Controller.tsx
@@ -9,16 +9,16 @@ type ControllerProps = {
 
 const Controller = (props: ControllerProps) => {
   return (
-    <div className="flex flex-col">
-      <div className="flex items-center">
+    <div className="flex-col">
+      <div>
         <input type="checkbox" className="mr-2" id="mute" checked={props.mute} onChange={() => {props.setMute(!props.mute)}} />
         <label htmlFor="mute">mute</label>
       </div>
-      <div className="flex items-center">
+      <div>
         <input type="checkbox" className="mr-2" id="compress" checked={props.compress} onChange={() => {props.setComporess(!props.compress)}} />
         <label htmlFor="compress">compress</label>
       </div>
-      <div className="flex items-center">
+      <div>
         <input type="checkbox" className="mr-2" id="gif" checked={props.gif} onChange={() => {props.setGif(!props.gif)}} />
         <label htmlFor="gif">gif</label>
       </div>
