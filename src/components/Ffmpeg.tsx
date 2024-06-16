dfb12d992982badabe9b21cf84da84476c86c24f
gb10udn <rbbpx0926@gmail.com>
2024-06-16 14:59:05 +0900

    update ffmpeg downloading information UI

--- src/components/Ffmpeg.tsx
+++ src/components/Ffmpeg.tsx
@@ -4,12 +4,14 @@ import { invoke } from "@tauri-apps/api/tauri";
 const Ffmpeg = () => {
   const [downloaded, setDownloaded] = useState<boolean>(false);
   const [downloading, setDownloading] = useState<boolean>(false);
+  const [hasError, setHasError] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<String>('');
 
   useEffect(() => {
     invoke("check_ffmpeg_downloaded", {}).then((result) => {
       setDownloaded(result as boolean);
       if (result === true) {
-        console.log("already downloaded ffmpeg !!!")
+        console.log("already downloaded ffmpeg !!!");
 
       } else {
         if (downloading === false) {
@@ -18,23 +20,45 @@ const Ffmpeg = () => {
             console.log("Finished downloading ffmpeg !!!");
             setDownloaded(true);
             setDownloading(false);
+            setHasError(false);
           }).catch((err) => {
+            setErrorMessage(err);
             console.log(err);
+            setHasError(true);
             setDownloaded(false);
             setDownloading(false);
           });
         }
       }
     }).catch((err) => {
+      setErrorMessage(err);
       console.log(err);
+      setHasError(true);
       setDownloaded(false);
       setDownloading(false);
     })
   }, []);
 
   return (
-    // TODO: 240513 もう少し直感的な UI にすること。
-    <p>ffmpeg downloaded → {String(downloaded)} / downloading → {String(downloading)}</p>
+    <>
+      { downloaded &&
+        <div className="p-4 my-4 text-sm text-blue-800 rounded-lg bg-blue-50" role="alert">
+          <span className="font-medium">ffmpeg downloaded success !!!</span> Upload and edit your movie
+        </div>
+      }
+
+      { downloading && 
+        <div className="p-4 my-4 text-sm text-yellow-800 rounded-lg bg-yellow-50" role="alert">
+          <span className="font-medium">ffmpeg downloading ...</span> Please wait a minutes ...
+        </div>
+      }
+
+      { hasError &&
+        <div className="p-4 my-4 text-sm text-red-800 rounded-lg bg-red-50" role="alert">
+          <span className="font-medium">ffmpeg downloaded failure ...</span> {errorMessage}
+        </div>
+      }
+    </>
   )
 }
 
